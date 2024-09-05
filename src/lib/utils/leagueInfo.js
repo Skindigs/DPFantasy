5845b9b4c38485112cc706e3b6d0d46386333654
Skindigs <dville5@gmail.com>
2024-09-05 11:29:44 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -26,6 +26,7 @@ export const managers = [
     {
       "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "name": "Gerard Salerno",
+      "managerID": "863793745546579968",  // the user's manager ID,
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Florida", // (optional)
       "bio": "Gerry The Kid, a manager who needs no introduction.",
